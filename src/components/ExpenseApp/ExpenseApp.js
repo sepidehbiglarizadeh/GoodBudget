dc97c46a047eb3fdd08e66d364530931e6388cd2
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-10-31 16:25:32 +0330

    create state for transaction

--- src/components/ExpenseApp/ExpenseApp.js
+++ src/components/ExpenseApp/ExpenseApp.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Header from "../Header/Header";
 import OverView from "../OverView/OverView";
 
 const ExpenseApp = () => {
+  const [transaction, setTransaction] = useState([]);
+
+  const addTransaction = (formValues) => {
+    setTransaction([...transaction, { ...formValues, id: Date.now }]);
+  };
+
   return (
     <>
       <Header />
-      <OverView />
+      <OverView addTransaction={addTransaction} />
     </>
   );
 };
